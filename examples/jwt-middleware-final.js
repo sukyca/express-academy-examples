13346ad05b9a418ace3e2711744ba673ca588023
Antonio Suljic <antonio.suljic1@gmail.com>
2021-08-16 15:26:54 +0200

    Assignment solved

--- examples/jwt-middleware-final.js
+++ examples/jwt-middleware-final.js
@@ -1,70 +1,75 @@
 // Load environment variable from .env file if not supplied
-const path = require('path');
-require('dotenv').config({
-  path: (typeof process.env.DOTENV_PATH !== 'undefined')
-    ? path.resolve(process.cwd(), process.env.DOTENV_PATH)
-    : path.resolve(process.cwd(), '.env'),
+const path = require("path");
+require("dotenv").config({
+  path:
+    typeof process.env.DOTENV_PATH !== "undefined"
+      ? path.resolve(process.cwd(), process.env.DOTENV_PATH)
+      : path.resolve(process.cwd(), ".env"),
 });
 
 // --- Start ---
-const _ = require('lodash');
-const express = require('express');
-const morgan = require('morgan');
-const jwtExpress = require('express-jwt');
-const jwt = require('jsonwebtoken');
+const _ = require("lodash");
+const express = require("express");
+const morgan = require("morgan");
+const jwtExpress = require("express-jwt");
+const jwt = require("jsonwebtoken");
+const loadPrivateKey = require("./../src/utils/loadPrivateKey");
 
-const colors = require('colors');
-const { wrap } = require('./../src/utils/asyncMiddleware');
+const colors = require("colors");
+const { wrap } = require("./../src/utils/asyncMiddleware");
 
-const pretty = require('./../src/utils/pretty');
-const { ERROR_PAGE_NOT_FOUND, errorDescriptor } = require('./../src/errors');
-const { customCORSAndAuthErrorMiddleware } = require('./../src/utils/customMiddleware');
-const loadPrivateKey = require('./../src/utils/loadPrivateKey');
-
+const pretty = require("./../src/utils/pretty");
+const { ERROR_PAGE_NOT_FOUND, errorDescriptor } = require("./../src/errors");
 const {
-  certPrivate,
-  certPublic,
-} = loadPrivateKey();
+  customCORSAndAuthErrorMiddleware,
+} = require("./../src/utils/customMiddleware");
+
+const { certPrivate, certPublic } = loadPrivateKey();
 
 const app = express();
 
-app.use(morgan('combined'));
-app.disable('x-powered-by');
+app.use(morgan("combined"));
+app.disable("x-powered-by");
 
-app.get('/', (req, res) => {
-  res.send('Home Page')
-})
-
-app.get('/public-key', (req, res) => {
-  res.type('text/plain');
-  res.send(certPublic)
-})
-
-app.get('/sign-content/:id', (req, res) => {
-  res.type('text/plain');
-  const result = jwt.sign({
-    userId: req.params.id,
-  }, certPrivate, { algorithm: 'RS256'});
-  res.send(result)
+app.get("/", (req, res) => {
+  res.send("Home Page");
 });
 
-app.get('/users',
+app.get("/public-key", (req, res) => {
+  res.type("text/plain");
+  res.send(certPublic);
+});
+
+app.get("/sign-content/:id", (req, res) => {
+  res.type("text/plain");
+  const result = jwt.sign(
+    {
+      userId: req.params.id,
+    },
+    certPrivate,
+    { algorithm: "RS256" }
+  );
+  res.send(result);
+});
+
+app.get(
+  "/users",
   jwtExpress({
     secret: certPublic,
-    algorithms: ['RS256'],
-    requestProperty: 'auth', // Default is 'user'
+    algorithms: ["RS256"],
+    requestProperty: "auth", // Default is 'user'
   }),
   customCORSAndAuthErrorMiddleware,
   (req, res, next) => {
     if (!req.auth) {
       return res.sendStatus(403);
     }
-    next()
-  }, 
+    next();
+  },
   (req, res) => {
-  res.send(`Users Page! ${JSON.stringify(req.auth)}`);
-})
-
+    res.send(`Users Page! ${JSON.stringify(req.auth)}`);
+  }
+);
 
 // Handle 404 and 505
 // The 404 Route (ALWAYS Keep this as the last route)
@@ -84,31 +89,35 @@ app.use((error, req, res, next) => {
       ...parsed,
     };
   } catch (e) {
-    console.log('[API - ERROR] Unknown error, unable to parse the error key, code and message:'.bgYellow.black);
+    console.log(
+      "[API - ERROR] Unknown error, unable to parse the error key, code and message:"
+        .bgYellow.black
+    );
     console.error(error);
     errorOut.code = 500;
-    errorOut.key = 'INTERNAL_SERVER_ERROR';
+    errorOut.key = "INTERNAL_SERVER_ERROR";
     errorOut.message = error.message;
   }
 
   // Audit the API errors
   // Get IP
-  const ip = _.get(req, 'headers.x-forwarded-for') || _.get(req, 'connection.remoteAddress');
+  const ip =
+    _.get(req, "headers.x-forwarded-for") ||
+    _.get(req, "connection.remoteAddress");
 
   // Send the response to the client
-  res.setHeader('Content-Type', 'application/json');
-  res.status(errorOut.code).send(pretty({
-    error: 1,
-    errors: [
-      { ...errorOut },
-    ],
-    data: null,
-  }));
+  res.setHeader("Content-Type", "application/json");
+  res.status(errorOut.code).send(
+    pretty({
+      error: 1,
+      errors: [{ ...errorOut }],
+      data: null,
+    })
+  );
 });
-
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`🚀  Server ready at ${port}`);
-})
+});
--- src/index.js
+++ src/index.js
@@ -1,30 +1,105 @@
 // Load environment variable from .env file if not supplied
-const path = require('path');
-require('dotenv').config({
-  path: (typeof process.env.DOTENV_PATH !== 'undefined')
-    ? path.resolve(process.cwd(), process.env.DOTENV_PATH)
-    : path.resolve(process.cwd(), '.env'),
+const path = require("path");
+require("dotenv").config({
+  path:
+    typeof process.env.DOTENV_PATH !== "undefined"
+      ? path.resolve(process.cwd(), process.env.DOTENV_PATH)
+      : path.resolve(process.cwd(), ".env"),
 });
 
 // --- Start ---
-const express = require('express')
-const morgan = require('morgan');
+const _ = require("lodash");
+const express = require("express");
+const morgan = require("morgan");
 
-const app = express()
+const app = express();
 
-app.use(morgan('combined'));
+const pretty = require("./utils/pretty");
+const jwtExpress = require("express-jwt");
+const jwt = require("jsonwebtoken");
+const loadPrivateKey = require("./utils/loadPrivateKey");
 
-app.get('/', (req, res) => {
-  res.send('Home Page')
-})
+const { certPrivate, certPublic } = loadPrivateKey();
 
-app.get('/users', (req, res) => {
-  res.send('Users Page!')
-})
+app.use(morgan("combined"));
 
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.get(
+  "/my-profile",
+  jwtExpress({
+    secret: certPublic,
+    algorithms: ["RS256"],
+    requestProperty: "auth",
+  }),
+  (req, res, next) => {
+    if (!req.auth) {
+      return res.sendStatus(403);
+    }
+    next();
+  },
+  (req, res) => {
+    res.send("Home Page");
+  }
+);
+
+app.get("/sign/:id", (req, res) => {
+  res.type("text/plain");
+  const result = jwt.sign(
+    {
+      userId: req.params.id,
+    },
+    certPrivate,
+    { algorithm: "RS256" }
+  );
+  res.send(result);
+});
+
+app.get("/public", (req, res) => {
+  res.type("text/plain");
+  res.send(certPublic);
+});
+
+app.use((req, res, next) => {
+  next(new Error(errorDescriptor(ERROR_PAGE_NOT_FOUND)));
+});
+
+app.use((error, req, res, next) => {
+  let errorOut = {};
+  let parsed = {};
+  try {
+    parsed = JSON.parse(error.message);
+    errorOut = {
+      ...parsed,
+    };
+  } catch (e) {
+    console.log(
+      "[API - ERROR] Unknown error, unable to parse the error key, code and message:"
+    );
+    console.error(error);
+    errorOut.code = 500;
+    errorOut.key = "INTERNAL_SERVER_ERROR";
+    errorOut.message = error.message;
+  }
+
+  const ip =
+    _.get(req, "headers.x-forwarded-for") ||
+    _.get(req, "connection.remoteAddress");
+
+  res.setHeader("Content-Type", "application/json");
+  res.status(errorOut.code).send(
+    pretty({
+      error: 1,
+      errors: [{ ...errorOut }],
+      data: null,
+    })
+  );
+});
 
 const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`🚀  Server ready at ${port}`);
-})
+});
